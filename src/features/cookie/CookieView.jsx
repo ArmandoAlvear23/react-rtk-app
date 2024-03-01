b20d36fa7c8d4983d978d18e0f563bf8bb817452
Armando Alvear <armando.alvear01@outlook.com>
2024-03-01 16:34:35 -0600

    implement get state using useSelector react hook

--- src/features/cookie/CookieView.jsx
+++ src/features/cookie/CookieView.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 export const CookieView = () => {
+  const numOfCookies = useSelector((state) => state.cookie.numOfCookies);
   return (
     <div>
-        <h2>Number of Cookies - </h2>
+        <h2>Number of Cookies - {numOfCookies}</h2>
         <button>Order Cookie</button>
         <button>Restock Cookies</button>
     </div>
