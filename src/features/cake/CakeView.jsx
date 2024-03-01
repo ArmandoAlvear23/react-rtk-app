1e5b5baab4873b92ee7c8a4e2e4b87133b76315c
Armando Alvear <armando.alvear01@outlook.com>
2024-03-01 16:56:22 -0600

    add useState hooks to manage ordered and restock user input states

--- src/features/cake/CakeView.jsx
+++ src/features/cake/CakeView.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import { ordered, restocked } from "./cakeSlice";
 
 export const CakeView = () => {
   const numOfCakes = useSelector((state) => state.cake.numOfCakes);
   const dispatch = useDispatch();
-  
+
+  const [numToOrder, setNumToOrder] = useState(1);
+  const [numToRestock, setNumToRestock] = useState(1); 
+
   return (
     <div>
-        <h2>Number of cakes - {numOfCakes}</h2>
-        <button onClick={()=> dispatch(ordered())}>Order cake</button>
-        <button onClick={()=> dispatch(restocked(1))}>Restock cakes</button>
+        <h2>Number of Cakes - {numOfCakes}</h2>
+        <input 
+            type="number"
+            value={numToOrder}
+            onChange={e => setNumToOrder(parseInt(e.target.value))} 
+        />
+        <button onClick={()=> dispatch(ordered(numToOrder))}>Order cake</button>
+        <br />
+        <input 
+            type="number"
+            value={numToRestock}
+            onChange={e => setNumToRestock(parseInt(e.target.value))} 
+        />
+        <button onClick={() => dispatch(restocked(numToRestock))}>Restock cakes</button>
     </div>
   )
 }
