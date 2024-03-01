9a8a3f691873b2505ef2fb2739f28ba42b0a1bea
Armando Alvear <armando.alvear01@outlook.com>
2024-03-01 15:35:47 -0600

    add views for each feature

---
+++ src/features/cookie/CookieView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const CookieView = () => {
+  return (
+    <div>
+        <h2>Number of Cookies - </h2>
+        <button>Order Cookie</button>
+        <button>Restock Cookies</button>
+    </div>
+  )
+}
