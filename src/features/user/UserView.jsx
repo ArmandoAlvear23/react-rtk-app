0b1f18a2aa28b6d8093cfbb0619ce6206dd6779d
Armando Alvear <armando.alvear01@outlook.com>
2024-03-03 09:20:19 -0600

    add user view

--- src/features/user/UserView.jsx
+++ src/features/user/UserView.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from './userSlice';
 
 export const UserView = () => {
-  return (
+    const user = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+    console.log(user);
+  return (  
     <div>
         <h2>List of Users</h2>
+        {user.isLoading && <div>Loading...</div>}
+        {!user.isLoading && user.error ? <div>Error: {user.error}</div> : null}
+        {!user.isLoading && user.users.length ? (
+            <ul>
+                {
+                    user.users.map(user => (
+                        <li key={user.id}>{user.name}</li>
+                    ))
+                }
+            </ul>
+        ) : null}
     </div>
   )
 }
